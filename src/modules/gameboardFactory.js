e9b21a45eed2391342b1a64609ca3abf246bceb1
Relyful <13381270+Relyful@users.noreply.github.com>
2024-08-05 21:38:47 +0200

    Fixed a bug now it really returns 2 on duplicate :)

--- src/modules/gameboardFactory.js
+++ src/modules/gameboardFactory.js
@@ -1,4 +1,4 @@
-import shipFactory from "./shipFactory";
+import shipFactory from './shipFactory';
 
 export default class gameboard {
   #gameboard = Array.from({ length: 10 }, () => Array(10).fill(0));
@@ -6,9 +6,9 @@ export default class gameboard {
   #hit = [];
   #allShips = [];
 
-  gameboardStatus(x,y) {
+  gameboardStatus(x, y) {
     console.log(this.#gameboard[x][y]);
-  };
+  }
 
   placeShip(shipSize, orientation, startCoord1, startCoord2) {
     const newShip = new shipFactory(shipSize);
@@ -17,60 +17,61 @@ export default class gameboard {
 
     //Add all proposed coords for new ship to array to check for conflicts and out of bounds
     for (let i = 0; i < shipSize; i++) {
-      if (orientation === "vertical") {
-        proposedCells.push([startCoord1 + i, startCoord2])
-      } else if (orientation === "horizontal") {
-        proposedCells.push([startCoord1, startCoord2 + i])
-      }     
-    };
+      if (orientation === 'vertical') {
+        proposedCells.push([startCoord1 + i, startCoord2]);
+      } else if (orientation === 'horizontal') {
+        proposedCells.push([startCoord1, startCoord2 + i]);
+      }
+    }
 
     // Check for conflicts and out of bounds
     for (let i = 0; i < proposedCells.length; i++) {
       const element = proposedCells[i];
       if (element[0] < 0 || element[0] > 9) {
-          console.log("Ship out of bounds");
-          return 0;
+        console.log('Ship out of bounds');
+        return 0;
       } else if (element[1] < 0 || element[1] > 9) {
-          console.log("Ship out of bounds");
-          return 0;
+        console.log('Ship out of bounds');
+        return 0;
       } else if (this.#gameboard[element[0]][element[1]] !== 0) {
-          console.log("Ship conflict");
-          return 0;
+        console.log('Ship conflict');
+        return 0;
       }
-  }
+    }
 
     //Place ship to coresponding cells
-    proposedCells.forEach(element => {
+    proposedCells.forEach((element) => {
       this.#gameboard[element[0]][element[1]] = newShip;
     });
     return 1;
-  };
+  }
 
   receiveAttack(coord1, coord2) {
     if (this.#gameboard[coord1][coord2] !== 0) {
-      const ship = this.#gameboard[coord1][coord2];
-      if (this.#hit.includes([coord1, coord2])) {
+      const ship = this.#gameboard[coord1][coord2]; 
+      if (arrayIncludesSubArray(this.#hit, [coord1, coord2])) {
         return 2;
       } else {
         this.#hit.push([coord1, coord2]);
-        ship.hit();     
+        ship.hit();
         return true;
-      } 
+      }
     } else {
-      if (this.#missed.includes([coord1, coord2])) {
+      console.log(this.#missed);
+      if (arrayIncludesSubArray(this.#missed, [coord1, coord2])) {
         return 2;
       } else {
-      this.#missed.push([coord1, coord2]);
-      return false
+        this.#missed.push([coord1, coord2]);
+        return false;
       }
     }
-  };
+  }
 
   allSunk() {
     let status = true;
     this.#allShips.forEach((element) => {
       if (!element.sunk) {
-        status = false
+        status = false;
         return;
       }
     });
@@ -87,9 +88,17 @@ export default class gameboard {
 
   get missed() {
     return this.#missed;
-  };
+  }
 
   get hit() {
     return this.#hit;
   }
+};
+
+function arrayIncludesSubArray(arr, subArr) {
+  return arr.some(item => 
+      Array.isArray(item) &&
+      item.length === subArr.length &&
+      item.every((val, index) => val === subArr[index])
+  );
 }
